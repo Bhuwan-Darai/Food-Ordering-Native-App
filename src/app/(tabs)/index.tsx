5b6b06876af2b84b0da5fea57e691081b1de30e5
bhuwan-darai <dr.darai29@gmail.com>
2024-03-14 22:49:56 +0545

    Expo router screens

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,21 +1,5 @@
-import { View, StyleSheet, Image, FlatList } from "react-native";
+import { Redirect } from "expo-router";
 
-import Colors from "@/src/constants/Colors";
-import products from "@/assets/data/products";
-import ProductListItem from "@/src/components/ProductListItem";
-
-export default function MenuScreen() {
-  return (
-    <View>
-      <FlatList
-        data={products}
-        renderItem={({ item }) => <ProductListItem product={item} />}
-        numColumns={2}
-        contentContainerStyle={{ gap: 10, padding: 10 }}
-        columnWrapperStyle={{ gap: 10 }}
-      />
-    </View>
-  );
+export default function TabIndex() {
+  return <Redirect href={"/menu/"} />;
 }
-
-const styles = StyleSheet.create({});
---
+++ src/app/(tabs)/menu/index.tsx
@@ -0,0 +1,21 @@
+import { View, StyleSheet, Image, FlatList } from "react-native";
+
+import Colors from "@/src/constants/Colors";
+import products from "@/assets/data/products";
+import ProductListItem from "@/src/components/ProductListItem";
+
+export default function MenuScreen() {
+  return (
+    <View>
+      <FlatList
+        data={products}
+        renderItem={({ item }) => <ProductListItem product={item} />}
+        numColumns={2}
+        contentContainerStyle={{ gap: 10, padding: 10 }}
+        columnWrapperStyle={{ gap: 10 }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
